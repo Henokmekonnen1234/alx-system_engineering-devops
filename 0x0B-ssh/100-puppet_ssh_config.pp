18dbeb9f9003e43a887a99c1da9d0a2098fa8fe1
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-11-27 00:22:03 +0300

    task 4 completes

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,13 @@
-file { '~/.ssh/config':
-    ensure  => 'file',
-    content => "
-Host alxstudentserver
-  HostName 52.201.221.223
-  User ubuntu
-  IdentityFile ~/.ssh/school
-  PasswordAuthentication no
-",
-  }
+# sets up a client SSH configuration file so that we can connect to a server without using a password
+include stdlib
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true,
+}
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
