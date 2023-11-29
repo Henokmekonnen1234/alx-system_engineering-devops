4e4fa239cff9b4ca2a1665b2c442e836e2c98449
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-11-29 23:40:42 +0300

    task 0 completed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-file {'/tmp/school':
-	ensure => present,
+# creates school
+file {'school':
+	path => '/tmp/school',
 	mode => 0744,
 	owner => "www-data",
 	group => "www-data",
-	content => "I love puppet"
+	content => "I love puppet",
 }
