57ecf3795329131727e2205a4ed189237972533c
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-11-30 23:25:23 +0300

    task 0 completed

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-# this is used for showing size of body
-curl -w "%{size_download}" "$1"
+curl -s "$1" | wc -c
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#this will show only the body response
+curl -s "$1"
