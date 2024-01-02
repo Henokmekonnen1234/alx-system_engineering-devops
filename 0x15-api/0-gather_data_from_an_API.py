2c7f429c192518f331f04b355145b63f3884f2a3
Henokmekonnen1234 <henokmac2425@gmail.com>
2024-01-02 23:44:26 +0300

    task 0 comleted

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
-"""
-Module 0-gather_data_from_an_API.py
+"""Module 0-gather_data_from_an_API.py
 
 in this module using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
 
-from requests import get
-from sys import argv
-from json import loads, load
-
-url_1 = "https://jsonplaceholder.typicode.com/todos/"
-url_2 = "https://jsonplaceholder.typicode.com/users/"
-value = {1: {"userId": argv[1]}, 2: {"id": argv[1]}}
-count = 0
-with get(url_1, params=value[1]) as response, get(url_2, params=value[2]) as u:
-    if response.status_code == 200 and u.status_code == 200:
-        value = loads(response.text)
-        user = loads(u.text)
-    else:
-        value = {}
-
-if value:
-    for todos in value:
-        if todos["completed"] is True:
-            count += 1
-    print("Employee {} is done with tasks({}/{}):".format(
-          user[0].get("name"), count, len(value)))
-    for todos in value:
-        if todos.get("completed") is True:
-            print(" \t{}".format(todos.get("title")))
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv
+    from json import loads, load
+    
+    url_1 = "https://jsonplaceholder.typicode.com/todos/"
+    url_2 = "https://jsonplaceholder.typicode.com/users/"
+    value = {1: {"userId": argv[1]}, 2: {"id": argv[1]}}
+    count = 0
+    with get(url_1, params=value[1]) as response, get(url_2, params=value[2]) as u:
+        if response.status_code == 200 and u.status_code == 200:
+            value = loads(response.text)
+            user = loads(u.text)
+        else:
+            value = {}
+    if value:
+        for todos in value:
+            if todos["completed"] is True:
+                count += 1
+        print("Employee {} is done with tasks({}/{}):".format(
+              user[0].get("name"), count, len(value)))
+        for todos in value:
+            if todos.get("completed") is True:
+                print(" \t{}".format(todos.get("title")))
